898dbc9aaaa88c4ae19426922fb385e1c5ff41d2
mannym1223 <mannymunoz1223@live.com>
2025-02-23 04:18:40 -0600

    Finalized scene for release. Added exit game function

--- Assets/Scripts/Utilities/SceneUtils.cs
+++ Assets/Scripts/Utilities/SceneUtils.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,5 +9,13 @@ public class SceneUtils : MonoBehaviour
     public void ReloadCurrentScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#endif
     }
 }
