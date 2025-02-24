1508f263aa2ce0a5daec0ea29132f2f8fad14dec
mannym1223 <mannymunoz1223@live.com>
2025-02-24 11:42:29 -0600

    Fixed timeline audio not playing in webgl builds due to Unity bug. Changed quit functionality for webgl. Added delay to reload and quit buttons.

--- Assets/Scripts/Utilities/SceneUtils.cs
+++ Assets/Scripts/Utilities/SceneUtils.cs
@@ -6,16 +6,31 @@ using UnityEngine.SceneManagement;
 
 public class SceneUtils : MonoBehaviour
 {
-    public void ReloadCurrentScene()
+    public void ReloadCurrentScene(float delay = 0.5f)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        StartCoroutine(BeginReloadingScene(delay));
     }
 
-    public void ExitGame()
+    public void ExitGame(float delay = 0.5f)
     {
-        Application.Quit();
-#if UNITY_EDITOR
-        EditorApplication.ExitPlaymode();
+        StartCoroutine(BeginExiting(delay));
+	}
+
+    private IEnumerator BeginReloadingScene(float delay) 
+    {
+		yield return new WaitForSeconds(delay);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+    private IEnumerator BeginExiting(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+#if (UNITY_EDITOR)
+		EditorApplication.ExitPlaymode();
+#elif (UNITY_STANDALONE)
+    Application.Quit();
+#elif (UNITY_WEBGL)
+    Application.OpenURL("https://itch.io/jam/brackeys-13/rate/3349706");
 #endif
     }
 }
